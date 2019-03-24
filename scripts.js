7835344cb9b16dc889299e1e22099d6c7ed43500
parrot <tarunkumargupta14@gmail.com>
2019-03-24 11:23:57 -0400

    monero added

--- scripts.js
+++ scripts.js
@@ -4,11 +4,14 @@ function loadWebAssembly(fileName) {
   return fetch(fileName)
     .then(response => response.arrayBuffer())
     .then(buffer => WebAssembly.compile(buffer))
-    .then(module => {return new WebAssembly.Instance(module) });
-};
-  
-loadWebAssembly('squarer.wasm')
-  .then(instance => {
-    squarer = instance.exports._Z7squareri;
-    console.log('Finished compiling! Ready when you are...');
-  }); 
+    .then(module => {
+      return new WebAssembly.Instance(module);
+    });
+}
+
+loadWebAssembly("simple.wasm").then(instance => {
+  const seed = "9f9ad282eec2f01adf6d5b947eb28225";
+  const mnemonic = instance.exports.wallet_mnemonic_from_seed(seed);
+
+  console.log(mnemonic);
+});
